38f4f4065d803005b79f21d79afb359fb6c2b6d9
elcanmuradov <elcanmuradov25@gmail.com>
2026-03-28 18:08:18 +0400

    2

---
+++ init-databases.sql
@@ -0,0 +1,13 @@
+-- init-databases.sql
+
+-- User Service Database
+CREATE DATABASE user_service;
+
+-- Product Service Database
+CREATE DATABASE product_service;
+
+-- Chat Service Database
+CREATE DATABASE chat_service;
+
+-- Media Service Database
+CREATE DATABASE media_service;
