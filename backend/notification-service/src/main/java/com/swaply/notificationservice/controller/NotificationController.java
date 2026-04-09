ed53ac937035b7f257716f51ad9caf02ca02f5a4
elcanmuradov <elcanmuradov25@gmail.com>
2026-04-09 18:49:35 +0400

    2

--- backend/notification-service/src/main/java/com/swaply/notificationservice/controller/NotificationController.java
+++ backend/notification-service/src/main/java/com/swaply/notificationservice/controller/NotificationController.java
@@ -2,15 +2,17 @@ package com.swaply.notificationservice.controller;
 
 import com.swaply.notificationservice.dto.VerificationRequest;
 import com.swaply.notificationservice.service.NotificationService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
 public class NotificationController {
     private final NotificationService notificationService;
+
+    public NotificationController(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
 
     @PostMapping("/user/send")
     public void sendNotification(@RequestBody VerificationRequest request) {
--- backend/notification-service/src/main/java/com/swaply/notificationservice/service/NotificationService.java
+++ backend/notification-service/src/main/java/com/swaply/notificationservice/service/NotificationService.java
@@ -3,35 +3,62 @@ package com.swaply.notificationservice.service;
 import com.swaply.notificationservice.dto.VerificationRequest;
 import com.swaply.notificationservice.exception.NotificationException;
 import com.swaply.notificationservice.utils.constants.EmailContext;
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.core.exception.SdkException;
+import software.amazon.awssdk.services.sesv2.SesV2Client;
+import software.amazon.awssdk.services.sesv2.model.Body;
+import software.amazon.awssdk.services.sesv2.model.Content;
+import software.amazon.awssdk.services.sesv2.model.Destination;
+import software.amazon.awssdk.services.sesv2.model.EmailContent;
+import software.amazon.awssdk.services.sesv2.model.Message;
+import software.amazon.awssdk.services.sesv2.model.SendEmailRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
-@Slf4j
-@RequiredArgsConstructor
 @Service
 public class NotificationService {
 
-    private final JavaMailSender javaMailSender;
+    private static final Logger log = LoggerFactory.getLogger(NotificationService.class);
+
+    private final SesV2Client sesV2Client;
+
+    @Value("${FROM_EMAIL}")
+    private String fromEmail;
+
+    public NotificationService(SesV2Client sesV2Client) {
+        this.sesV2Client = sesV2Client;
+    }
 
     public void sendVerificationEmail(VerificationRequest request) {
         try {
-            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+        SendEmailRequest emailRequest = SendEmailRequest.builder()
+            .fromEmailAddress(fromEmail)
+            .destination(Destination.builder()
+                .toAddresses(request.getEmail())
+                .build())
+            .content(EmailContent.builder()
+                .simple(Message.builder()
+                    .subject(Content.builder()
+                        .data("Your verification code is...")
+                        .charset("UTF-8")
+                        .build())
+                    .body(Body.builder()
+                        .html(Content.builder()
+                            .data(EmailContext.setToken(request.getToken()))
+                            .charset("UTF-8")
+                            .build())
+                        .build())
+                    .build())
+                .build())
+            .build();
 
-            helper.setTo(request.getEmail());
-            helper.setSubject("Your verification code is...");
-            helper.setText(EmailContext.setToken(request.getToken()), true);
-            javaMailSender.send(mimeMessage);
-            log.info("Email has been sent");
+        sesV2Client.sendEmail(emailRequest);
+        log.info("Email has been sent to {}", request.getEmail());
 
-        } catch (MessagingException e) {
-            throw new NotificationException(e.getMessage());
+    } catch (SdkException e) {
+        throw new NotificationException(e.getMessage());
         }
     }
 
